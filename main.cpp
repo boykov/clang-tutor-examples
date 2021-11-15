2f3c85791f00d5933bf993ea89e2072d322be332
Evgeny Boykov <>
2021-11-15 01:41:18 +0000

    copy matrix

--- main.cpp
+++ main.cpp
@@ -2,19 +2,39 @@
 #include <iostream>
 using namespace std;
  
-// function to find factorial of given number
-unsigned int factorial(unsigned int n)
-{
-    if (n == 0)
-        return 1;
-    return n * factorial(n - 1);
-}
- 
-// Driver code
 int main()
 {
-    int num = 6;
-    cout << "Factorial of "
-         << num << " is " << factorial(num) << endl;
+    int array[36] = {1,1,1,1,1,1,
+                     2,2,2,2,2,2,
+                     3,3,3,3,3,3,
+                     4,4,4,4,4,4,
+                     5,5,5,5,5,5,
+                     6,6,6,6,6,6};
+    int copyarray[36];
+
+    for (int i = 0; i < 6; ++i)
+        for (int j = 0; j < 6; ++j)
+            copyarray[i*6+j]= array[j*6+i];
+
+    for (int i = 0; i < 36; ++i)
+    {
+        if (i % 6 == 0)
+            printf("\n");
+
+        printf("%d ", array[i]);
+
+    }
+
+    printf("\n");
+    printf("\n");
+
+    for (int i = 0; i < 36; ++i)
+    {
+        if (i % 6 == 0)
+            printf("\n");
+
+        printf("%d ", copyarray[i]);
+    }
+
     return 0;
 }
